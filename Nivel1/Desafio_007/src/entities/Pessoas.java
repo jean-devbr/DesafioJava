e670fd107e5a3ab613a265601681a4d0add3a601
jean costa <costajean1005@gmail.com>
2025-08-09 20:20:37 -0300

    Encontrando Nome digitando

---
+++ Nivel1/Desafio_007/src/entities/Pessoas.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Pessoas {
+	private String nome;
+
+	public Pessoas(String nome) {
+		
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
